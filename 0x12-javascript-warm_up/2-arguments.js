c3e22fa2d1a3bf82a63e848b622c080e53ba0bf2
lchourouk <loubnachourouk@gmail.com>
2024-06-21 07:51:20 -0700

    argument

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,4 @@
 #!/usr/bin/node
 
-const { argv } = require('node-process');
-const count = argv.length;
-if (count === 0) {
-  console.log('No argument');
-} else if (count === 1) {
-  console.log('Argument found');
-} else console.log('Arguments found');
+const count = process.argv.length - 2;
+if (count == 0) { console.log('No argument'); } else if (count == 1) { console.log('Argument found'); } else console.log('Arguments found');
